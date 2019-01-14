76c1f020766b788f6a747fe36649c63522360fb6
Rhayven <amayis@ymail.com>
2019-01-14 01:37:12 -0500

    Better formatting to accomodate planned changes

--- PropegationOfErrorsSolver.py
+++ PropegationOfErrorsSolver.py
@@ -4,7 +4,12 @@
 # This program works to solve propegation of errors in equations using input
 # From the end user as variables.
 #
+# Currently, it solves Multiplication based Propegation of Errors.
+# 
+# Formulas Used:
 #
+# Multiplication: (x*y) + (x*error Y) + (Y * error X)
+# 
 # Thanks to @JulianaTh for helping with the equations
 
 import math
@@ -14,8 +19,10 @@ y = float(input("Please enter the Y variable: "))
 errX = float(input("Please enter the X Error Variable: "))
 errY = float(input("Please enter the Y Error Variable: "))
 
-total_error1 = x*y
-total_error2 = ((x*errY) + (y*errX))
-final_error = str(total_error1) + " +- " + str(total_error2)
+product = x*y
 
-print("Final Result:" + " +- " + final_error)
+multiplication_error = ((x*errY) + (y*errX))
+
+multiplication_total = str(product) + " +- " + str(multiplication_error)
+
+print("Final Result: " + multiplication_total)
